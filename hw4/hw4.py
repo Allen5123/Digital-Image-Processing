77e601bb538076ac2172c4c646b34bf00a181aff
Allen5123 <allen23648858@gmail.com>
2022-09-24 16:10:32 +0800

    finish hw4 prob1 (c)

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,3 +1,4 @@
+from locale import normalize
 import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
@@ -48,6 +49,47 @@ def Dithering(img, dsize):
                         ret[i+k,j+l] = 0
     return ret
 
+@jit
+def MirrorPadding(img, ksize):
+    m, n = img.shape
+    s, t = ksize
+    #mirror padding
+    padded = np.zeros((m+(s//2)*2, n+(t//2)*2), dtype=np.int32)
+    padded[s//2:s//2+m, t//2:t//2+n] = img[:,:]
+    padded[:s//2,t//2:t//2+n] = padded[(s//2)*2:s//2:-1,t//2:t//2+n]
+    padded[-1:-(s//2+1):-1,t//2:t//2+n] = padded[-((s//2)*2+1):-(s//2+1),t//2:t//2+n]
+    padded[:,:t//2] = padded[:,2*(t//2):t//2:-1]
+    padded[:,-1:-(t//2+1):-1] = padded[:,-(2*(t//2)+1):-(t//2+1)]
+    return padded
+
+@jit
+def ErrorDiffusion(img, pattern):
+    errormatrix = img.astype(np.float32)
+    m, n = img.shape
+    ltor, rtol = pattern, np.fliplr(pattern)
+    for i in range(m):
+        if i % 2 == 0:
+            for j in range(n):
+                newpixel = 255 * np.round(errormatrix[i,j] / 255)
+                error = errormatrix[i,j] - newpixel
+                errormatrix[i,j] = newpixel
+                s, t = ltor.shape
+                for k in range(-(s//2), s//2+1):
+                    for l in range(-(t//2), t//2+1):
+                        if i + k >= 0 and i + k < m and j + l >= 0 and j + l < n:
+                            errormatrix[i+k,j+l] += error * ltor[k+s//2,l+t//2]
+        else:
+            for j in range(n-1,-1,-1):
+                newpixel = 255 * np.round(errormatrix[i,j] / 255)
+                error = errormatrix[i,j] - newpixel
+                errormatrix[i,j] = newpixel
+                s, t = rtol.shape
+                for k in range(-(s//2), s//2+1):
+                    for l in range(-(t//2), t//2+1):
+                        if i + k >= 0 and i + k < m and j + l >= 0 and j + l < n:
+                            errormatrix[i+k,j+l] += error * rtol[k+s//2,l+t//2]
+    return errormatrix.astype(np.uint8)
+
 def Prob1():
     sample1 = cv.imread(pathsep.join([".", "hw4_sample_images", "sample1.png"]), cv.IMREAD_GRAYSCALE)
     if sample1 is None:
@@ -56,5 +98,11 @@ def Prob1():
     cv.imwrite(pathsep.join([".", "result1.png"]), res1)
     res2 = Dithering(sample1, 256)
     cv.imwrite(pathsep.join([".", "result2.png"]), res2)
+    res3 = ErrorDiffusion(sample1, np.array([[0.,0.,0.],[0.,0.,7.],[3.,5.,1.]])/16.)
+    cv.imwrite(pathsep.join([".", "result3.png"]), res3)
+    res4 = ErrorDiffusion(sample1, np.array([[0.,0.,0.,0.,0.],[0.,0.,0.,0.,0.],[0.,0.,0.,7.,5.],[3.,5.,7.,5.,3.],[1.,3.,5.,3.,1.]])/48.)
+    cv.imwrite(pathsep.join([".", "result4.png"]), res4)
+    atkinson = ErrorDiffusion(sample1, np.array([[0.,0.,0.,0.,0.],[0.,0.,0.,0.,0.],[0.,0.,0.,1.,1.],[0.,1.,1.,1.,0.],[0.,0.,1.,0.,0.]])/8.)
+    cv.imwrite(pathsep.join([".","atkinson.png"]), atkinson)
 
 Prob1()
